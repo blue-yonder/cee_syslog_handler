d9ab6ca4626ceb83bb6b501ff966157e3f2a791d
Manuel Bähr <manuel.baehr@blue-yonder.com>
2015-03-05 09:46:14 +0100

    added failing test for non-string types

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,9 +1,27 @@
 from cee_syslog_handler import get_fields
 
+
 class Record(object):
-    pass
+    def __init__(self, value=None):
+        if value:
+            self.some_column = value 
+
+
+def check_single_value(value):
+    record = Record(value)
+    message_dict = get_fields({}, record)
+    assert message_dict == {'_some_column': value}
 
 def test_get_fields_empty():
     record =  Record()
     message_dict = get_fields({}, record)
     assert message_dict == {}
+
+def test_string_types():
+    check_single_value('some_text')
+
+def test_float_types():
+    check_single_value(1.1)
+
+def test_int_types():    
+    check_single_value(1)
