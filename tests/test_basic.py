1b05811d474628fda9175f3d740843d395087c63
Manuel Bähr <manuel.baehr@blue-yonder.com>
2015-03-05 09:58:04 +0100

    generic support for numeric types

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -19,9 +19,22 @@ def test_get_fields_empty():
 
 def test_string_types():
     check_single_value('some_text')
+    check_single_value(u'some_text')
 
-def test_float_types():
+def test_numeric_types():
+    pi = 3.1415
+
+    class SomeType(object):
+        def __init__(self):
+            self._value = pi
+
+        def __float__(self):
+            return self._value
+
+        def __eq__(self, value):
+            return float(value) == self._value
+
     check_single_value(1.1)
-
-def test_int_types():    
     check_single_value(1)
+    assert float(SomeType()) == pi
+    check_single_value(SomeType())
